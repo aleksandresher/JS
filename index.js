2c7651e70ea9c74ea17ad3aea051ef49fd243b83
Aleksandre Shervashidze <aleksandre.shervashidze.2@iliauni.edu.ge>
2023-11-27 14:59:32 +0400

    numbers and number methods

---
+++ index.js
@@ -0,0 +1,42 @@
+////////////////// Numbers
+
+let num = 3;
+let newNum = 3.16;
+
+console.log(typeof num);
+console.log(typeof newNum);
+
+let mill = 1e6;
+console.log(mill); /// 1000000
+
+let smallNum = 1e-6;
+console.log(smallNum); /// 0.000001
+
+let score = new Number(100);
+console.log(typeof score); // object
+
+let testNum = 1e5;
+console.log(testNum); // 100000
+console.log(typeof testNum); // number
+
+//////////// Number Methods////////////////
+
+const PI = 3.141596254;
+
+let num2 = PI.toFixed(2);
+console.log(num2); /// 3.14
+
+num2 = PI.toPrecision(5);
+console.log(num2); // 3.1416
+
+let mill2 = 1000000;
+num2 = mill.toExponential();
+console.log(num2); // 1e+6
+
+let PI1 = 3.14159;
+console.log(PI1.toFixed(3)); //3.142
+
+console.log(PI1.toPrecision(3)); /// 3.14
+
+const lightSpeed = 186000;
+console.log(lightSpeed.toExponential()); // 1.86+5
