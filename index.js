2ef83eb0bd51dccd442f019cf88c540f11658eef
Aleksandre Shervashidze <aleksandre.shervashidze.2@iliauni.edu.ge>
2023-12-19 19:26:23 +0400

    callback functions

--- index.js
+++ index.js
@@ -1737,3 +1737,27 @@ console.log(b4()); // ƒ xyz() {}
 
 /////////////////////
 /////////////////////// callback function
+
+setTimeout(function () {
+  console.log("times");
+}, 5000);
+
+function x(y) {
+  console.log("x");
+  y();
+}
+x(function y() {
+  console.log("y");
+});
+
+//////////////
+//////////////
+
+function attachEventListener() {
+  let count = 0;
+  document.getElementById("clickMe").addEventListener("click", function xyz() {
+    console.log("button clicked", ++count);
+  });
+}
+
+attachEventListener();
