c98541e60619f726f8174d4cb3df2d765059b617
Aleksandre Shervashidze <aleksandre.shervashidze.2@iliauni.edu.ge>
2023-12-29 01:25:48 +0400

    url-request

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 ////////////////// Numbers
 
+const { response } = require("express");
+
 // let num = 3;
 // let newNum = 3.16;
 
@@ -2210,17 +2212,59 @@ console.log("Main code completed");
 
 ///////////////////
 /////////////////////////////// try-async
-const url = "https://jsonplaceholder.typicode.com/users";
+// const url = "https://jsonplaceholder.typicode.com/users";
 
-async function getData() {
-  try {
-    let response = await fetch(url);
-    console.log(response);
-    if (!response.ok) throw new Error("not a valid response");
-    let dataobj = await response.json();
-    console.log(dataobj);
-  } catch (err) {
-    console.warn(err.message);
-  }
+// async function getData() {
+//   try {
+//     let response = await fetch(url);
+//     console.log(response);
+//     if (!response.ok) throw new Error("not a valid response");
+//     let dataobj = await response.json();
+//     console.log(dataobj);
+//   } catch (err) {
+//     console.warn(err.message);
+//   }
+// }
+// getData();
+
+////////////////
+///////////////////////////// url-request
+
+/*
+ url: href, host, hostname, port, protocol, origin, pathname, hash, search, searchParams
+request options: method, body, headers, cache
+cache  (HTTP Cache, NOT Cache API)
+- `default`: cache first, server request if stale, update cache if newer
+- `reload`: always go to server AND update the cache
+- `no-store`: always go to server but do not update the cache
+- `no-cache`: make a conditional request to server and compare, update cache and use latest
+- `force-cache`: only makes request if there is no HTTP Cache file
+- `only-if-cache`: from cache or 504 gateway timeout error
+Headers
+- string | object literal | new Headers()
+*/
+
+const str = "http://127.0.0.1:5500/local-sample.json?attempt=123&other=hello";
+
+function getData() {
+  const url = new URL(str);
+  // console.log(url.host, url.origin, url.protocol, url.port, url.pathname);
+
+  const request = new Request(url, {
+    headers: { "x-steve": "hello" },
+    method: "GET",
+    cache: "no-store",
+  });
+
+  fetch(request)
+    .then((response) => {
+      // console.log(response.status);
+      if (!response.ok) throw new Error("Invalid");
+      return response;
+    })
+    .then((data) => {
+      console.log(data);
+    })
+    .catch((err) => console.warn(err.message));
 }
 getData();
