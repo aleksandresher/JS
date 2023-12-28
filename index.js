eaa9f41a18a0cdf3a59025d1d095a29b97c5e18d
Aleksandre Shervashidze <aleksandre.shervashidze.2@iliauni.edu.ge>
2023-12-28 20:43:19 +0400

    async/callbacks

--- index.js
+++ index.js
@@ -2077,3 +2077,32 @@ console.log("Main code completed");
 /// Main code completed
 // Task2 - completed
 // Task1 - completed
+
+///////////////////////////////////////////////// Async ///////////////////////////////////////
+////////////////////////// callbacks
+
+const posts = [
+  { title: "Post One", body: "This is post one" },
+  { title: "Post Two", body: "This is post two" },
+];
+
+function getPosts() {
+  setTimeout(() => {
+    let output = "";
+    posts.forEach((post, index) => {
+      output += `<li>${post.title}</li>`;
+    });
+    document.body.innerHTML = output;
+  }, 1000);
+}
+
+function createPost(post, callback) {
+  setTimeout(() => {
+    posts.push(post);
+    callback();
+  }, 2000);
+}
+
+getPosts();
+
+createPost({ title: "Post Three", body: "This is post three" }, getPosts);
