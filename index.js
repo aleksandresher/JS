e9f74167ce7af7ad2a9bb756d09a7e10d8862044
Aleksandre Shervashidze <aleksandre.shervashidze.2@iliauni.edu.ge>
2024-01-03 14:50:34 +0400

    multiple/abort

--- index.js
+++ index.js
@@ -2336,150 +2336,220 @@ Headers
 
 ///////////////////////////// API Keys, Authorization, Credentials, Content-Security-Policy
 
+// function getData() {
+//   let str = "http://127.0.0.1:3000/?name=value&steve=griffith";
+//   let url = new URL(str); //url.search
+//   let sp = url.searchParams;
+//   sp.append("hello", "world");
+//   sp.append("api-key", "sdfsdfert34rwsedweferf");
+//   // document.cookie('')
+
+//   let h = new Headers();
+//   // h.append('content-type', 'application/json')
+//   // h.append('origin', 'https://cia.org')
+//   h.append("x-api-key", "lijhkjehrkwjehrkwjer"); //API key
+//   h.append("Authorization", "liuhsgdigfyksdjfksdlfsddfdf"); //JWT
+//   //Forbidden Header Names
+
+//   let request = new Request(url, {
+//     method: "GET",
+//     headers: h,
+//     cache: "default",
+//     credentials: "same-origin",
+//   });
+
+//   fetch(request)
+//     .then((response) => {
+//       if (!response.ok) throw new Error("invalid");
+//       return response.text();
+//     })
+//     .then((txt) => {
+//       console.log(txt);
+//     })
+//     .catch(console.warn);
+// }
+
+// //////////////////////////////////// Uploading Data//////////////////////////////////
+
+// let endpoint = "http://127.0.0.1:3000/";
+
+// function setData() {
+//   const imgInput = document.getElementById("imgfile");
+//   const jsonInput = document.getElementById("jsonfile");
+
+//   document.getElementById("myform").addEventListener("submit", (ev) => {
+//     ev.preventDefault();
+//     //upload something
+//     let obj = {
+//       id: 123,
+//       name: "steve",
+//     };
+//     // let jsonstring = JSON.stringify(obj);
+
+//     let fd = new FormData(document.getElementById("myform"));
+
+//     let request = new Request(endpoint, {
+//       method: "POST",
+//       // body: jsonstring,
+//       body: fd,
+//       headers: {
+//         "content-type": "multipart/form-data",
+//       },
+//     });
+//     fetch(request)
+//       .then((response) => {
+//         if (!response.ok) throw new Error("invalid");
+//         return response.text();
+//       })
+//       .then((txt) => {
+//         console.log(txt);
+//       })
+//       .catch(console.warn);
+//   });
+// }
+
+// /////////////////////////////////// Cors ///////////////////////////////////////////
+// /*
+// All requests that are not GET or HEAD need to include an `origin` header.
+// Set by the browser, not the script.
+// Server sets ACCESS-Control-Allow-Origin: http://127.0.0.1:5500
+
+// Simple Request: The preflight request is NOT needed when:
+
+// 1. method is HEAD, GET, or POST;
+// 2. and headers are only accept, accept-language, content-language, content-type, or range
+// 3. and content-type is only `text/plain`, `multipart/form-data`, or `application/x-www-form-urlencoded`;
+// 4. and `accept`, `accept-language`, and `content-language` are only standard values;
+// 5. and no ReadableStream object is used in the Request
+
+// CORS OPTIONS request includes the
+// `Access-Control-Request-Method` and
+// `Access-Control-Request-Headers` headers.
+
+// Request Mode
+// - no-cors: skip the OPTIONS request
+// - cors: must meet the CORS criteria. OPTIONS request will be made
+// - same-origin: result in an error if not same origin
+// - navigate: only set by the browser, not by JS
+
+// Opaque responses can be used as the contents of a `<script>`, `<link rel="stylesheet">`, `<img>`, `<video>`, `<audio>`, `<iframe>`, `<embed>` or `<object>`.
+
+// Not for `<canvas>`.
+
+// Not for Web Fonts.
+
+// Not for Cache Storage `add()` or `addAll()`. But you can do a fetch and then use Cache `put()`.
+
+// Opaque responses have a status of 0.
+
+// Using no-cors mode basically means that, if you get an opaque response:
+
+// 1. you don't care about seeing the resulting file
+// 2. don't bother sending a preflight request.
+
+// A CORS-safelisted response-header names
+
+// `Cache-Control` `Content-Language` `Content-Length` `Content-Type` `Expires` `Last-Modified` `Pragma`
+// */
+
+// function getData() {
+//   const imgurl = `https://picsum.photos/id/237/300/200`;
+
+//   let request = new Request(imgurl, {
+//     cache: "default",
+//     credentials: "same-origin",
+//     method: "GET",
+//     mode: "no-cors",
+//   });
+
+//   fetch(imgurl).then((resp) => {
+//     console.log("External IMG mode: cors");
+//     console.log(resp.status); // 200
+//   });
+//   fetch(imgurl, { mode: "no-cors" }).then((resp) => {
+//     console.log("External IMG mode: no-cors");
+//     console.log(resp.status); // 0
+//   });
+
+//   const jsonurl = `http://127.0.0.1:5500/local-sample.json`;
+//   fetch(jsonurl, { mode: "cors" }).then((resp) => {
+//     console.log("Local JSON mode: cors");
+//     console.log(resp.status); // 200
+//   });
+//   fetch(jsonurl, { mode: "no-cors" }).then((resp) => {
+//     console.log("Local JSON mode: no-cors");
+//     console.log(resp.status); // 0
+//   });
+// }
+// getData();
+
+///////////////////////////////////// multiple requests //////////////////////////////
+// const jsonstr = "https://random-data-api.com/api/v2/users?size=10";
+// const imgstr = "https://picsum.photos/id/237/300/200";
+
+// function getData() {
+//   //
+//   let imgResponse;
+//   let jsonResponse;
+//   fetch(imgstr)
+//     .then((response) => {
+//       if (!response.ok) throw new Error("invalid");
+//       return response.blob();
+//       //we can add another then and work with image here
+//     })
+//     .then((blob) => {
+//       //work with blob add img to page...
+//       return fetch(jsonstr);
+//     })
+//     .then((response) => {
+//       if (!response.ok) throw new Error("invalid");
+//       jsonResponse = response.json();
+//       return Promise.all([imgResponse, jsonResponse]);
+//     })
+//     .then(([blob, dataObj]) => {
+//       console.log(dataObj);
+//       console.log(blob);
+//     })
+//     .catch(console.warn);
+
+//   Promise.all([fetch(imgstr), fetch(jsonstr)])
+//     .then(([imgRes, jsonRes]) => {
+//       return Promise.all([imgRes.blob(), jsonRes.json()]);
+//     })
+//     .then(([blob, jsonData]) => {
+//       //at the same time
+//     })
+//     .catch(console.warn);
+// }
+
+// getData();
+
+//////////////////////////////////////////// Abort ///////////////////////////////////
+
+const url = "https://picsum.photos/id/237/3000/2000";
+
+const controller = new AbortController();
+const signal = controller.signal;
+
 function getData() {
-  let str = "http://127.0.0.1:3000/?name=value&steve=griffith";
-  let url = new URL(str); //url.search
-  let sp = url.searchParams;
-  sp.append("hello", "world");
-  sp.append("api-key", "sdfsdfert34rwsedweferf");
-  // document.cookie('')
-
-  let h = new Headers();
-  // h.append('content-type', 'application/json')
-  // h.append('origin', 'https://cia.org')
-  h.append("x-api-key", "lijhkjehrkwjehrkwjer"); //API key
-  h.append("Authorization", "liuhsgdigfyksdjfksdlfsddfdf"); //JWT
-  //Forbidden Header Names
-
-  let request = new Request(url, {
-    method: "GET",
-    headers: h,
-    cache: "default",
-    credentials: "same-origin",
+  let abortBtn = document.getElementById("abort");
+  abortBtn.addEventListener("click", (ev) => {
+    controller.abort();
+    console.log("aborted");
   });
 
+  let request = new Request(url, {
+    signal: signal,
+  });
   fetch(request)
     .then((response) => {
       if (!response.ok) throw new Error("invalid");
-      return response.text();
+      return response.blob();
     })
-    .then((txt) => {
-      console.log(txt);
+    .then((blob) => {
+      console.log("got the blob");
     })
     .catch(console.warn);
 }
 
-//////////////////////////////////// Uploading Data//////////////////////////////////
-
-let endpoint = "http://127.0.0.1:3000/";
-
-function setData() {
-  const imgInput = document.getElementById("imgfile");
-  const jsonInput = document.getElementById("jsonfile");
-
-  document.getElementById("myform").addEventListener("submit", (ev) => {
-    ev.preventDefault();
-    //upload something
-    let obj = {
-      id: 123,
-      name: "steve",
-    };
-    // let jsonstring = JSON.stringify(obj);
-
-    let fd = new FormData(document.getElementById("myform"));
-
-    let request = new Request(endpoint, {
-      method: "POST",
-      // body: jsonstring,
-      body: fd,
-      headers: {
-        "content-type": "multipart/form-data",
-      },
-    });
-    fetch(request)
-      .then((response) => {
-        if (!response.ok) throw new Error("invalid");
-        return response.text();
-      })
-      .then((txt) => {
-        console.log(txt);
-      })
-      .catch(console.warn);
-  });
-}
-
-/////////////////////////////////// Cors ///////////////////////////////////////////
-/*
-All requests that are not GET or HEAD need to include an `origin` header. 
-Set by the browser, not the script.
-Server sets ACCESS-Control-Allow-Origin: http://127.0.0.1:5500
-
-Simple Request: The preflight request is NOT needed when:
-
-1. method is HEAD, GET, or POST;
-2. and headers are only accept, accept-language, content-language, content-type, or range
-3. and content-type is only `text/plain`, `multipart/form-data`, or `application/x-www-form-urlencoded`;
-4. and `accept`, `accept-language`, and `content-language` are only standard values;
-5. and no ReadableStream object is used in the Request
-
-CORS OPTIONS request includes the 
-`Access-Control-Request-Method` and 
-`Access-Control-Request-Headers` headers.
-
-Request Mode
-- no-cors: skip the OPTIONS request
-- cors: must meet the CORS criteria. OPTIONS request will be made
-- same-origin: result in an error if not same origin
-- navigate: only set by the browser, not by JS
-
-
-Opaque responses can be used as the contents of a `<script>`, `<link rel="stylesheet">`, `<img>`, `<video>`, `<audio>`, `<iframe>`, `<embed>` or `<object>`.
-
-Not for `<canvas>`.
-
-Not for Web Fonts.
-
-Not for Cache Storage `add()` or `addAll()`. But you can do a fetch and then use Cache `put()`.
-
-Opaque responses have a status of 0.
-
-Using no-cors mode basically means that, if you get an opaque response:
-
-1. you don't care about seeing the resulting file
-2. don't bother sending a preflight request.
-
-A CORS-safelisted response-header names
-
-`Cache-Control` `Content-Language` `Content-Length` `Content-Type` `Expires` `Last-Modified` `Pragma`
-*/
-
-function getData() {
-  const imgurl = `https://picsum.photos/id/237/300/200`;
-
-  let request = new Request(imgurl, {
-    cache: "default",
-    credentials: "same-origin",
-    method: "GET",
-    mode: "no-cors",
-  });
-
-  fetch(imgurl).then((resp) => {
-    console.log("External IMG mode: cors");
-    console.log(resp.status); // 200
-  });
-  fetch(imgurl, { mode: "no-cors" }).then((resp) => {
-    console.log("External IMG mode: no-cors");
-    console.log(resp.status); // 0
-  });
-
-  const jsonurl = `http://127.0.0.1:5500/local-sample.json`;
-  fetch(jsonurl, { mode: "cors" }).then((resp) => {
-    console.log("Local JSON mode: cors");
-    console.log(resp.status); // 200
-  });
-  fetch(jsonurl, { mode: "no-cors" }).then((resp) => {
-    console.log("Local JSON mode: no-cors");
-    console.log(resp.status); // 0
-  });
-}
 getData();
